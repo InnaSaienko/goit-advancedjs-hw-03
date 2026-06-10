9e5b5e1eae8691c50c1575b3a1899222441f8a44
Inna Saienko <inna.saienko.nl@gmail.com>
2026-06-10 11:20:13 +0200

    feat: improve gallery items according task condition

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,58 +1,60 @@
-import iziToast from "izitoast";
-import SimpleLightbox from "simplelightbox";
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 
 //Show message form according state
-const errorMessage = "Sorry, there are no images matching your search query. Please try again!";
-const warningMessage = "Sorry, no images match your search. Try again!";
+const errorMessage = 'Sorry, there are no images matching your search query. Please try again!';
+const warningMessage = 'Sorry, no images match your search. Try again!';
+const lightbox = new SimpleLightbox('.gallery a');
+const gallery = document.querySelector('.gallery');
 
 export const showToast = (messageType) => {
   const config = {
-    title: messageType === "warning" ? warningMessage : errorMessage,
-    message: `${message}`,
-    position: 'topCenter',
-    timeout: 5000,
-  }
+    title: messageType === 'warning' ? warningMessage : errorMessage, position: 'topCenter', timeout: 5000,
+  };
+
   return messageType === 'warning' ? iziToast.warning(config) : iziToast.error(config);
 
-}
+};
 
 export function showLoader() {
-  const loader = document.createElement("div");
-  loader.classList.add("loader");
-  loader.textContent = "Loading images, please wait...";
+  const loader = document.createElement('div');
+  loader.classList.add('loader');
+  loader.textContent = 'Loading images, please wait...';
   document.body.appendChild(loader);
   return loader;
+
 }
 
 export function hideLoader() {
-  const loader = document.querySelector(".loader");
+  const loader = document.querySelector('.loader');
   loader.remove();
+
 }
 
 export function createGallery(images) {
-  const gallery = document.createElement("div");
-  const markUp = images.map(image =>
-    `<li className="gallery-item">
-      <a href={image.largeImageURL} className="gallery-link">
-        <img
-          src={image.webformatURL}
-          alt={image.tags}
-          className="gallery-image"
-        />
-        <div className="image-info">
-          <p><b>Likes:</b> ${image.likes}</p>
-          <p><b>Views:</b> ${image.views}</p>
-          <p><b>Comments:</b> ${image.comments}</p>
-          <p><b>Downloads:</b> ${image.downloads}</p>
-        </div>
-      </a>
-    </li>`
-  ).join("");
+  const markUp = images.map(image => {
+    return `<li class="gallery-item">
+        <a href=${image.largeImageURL} class="gallery-link">
+          <img
+            class="gallery-image"
+            src=${image.webformatURL}
+            alt=${image.tags}
+            loading="lazy"
+          />
+        </a>
+         <ul class="image-info">
+           <li class="info-item">Likes: <span>${image.likes}</span></li>
+           <li class="info-item">Views: <span>${image.views}</span></li>
+           <li class="info-item">Comments: <span>${image.comments}</span></li>
+           <li class="info-item">Download: <span>${image.downloads}</span></li>
+         </ul>
+      </li>`;
+  }).join('');
 
-  gallery.insertAdjacentHTML("beforeend", markUp);
-  const lightbox = new SimpleLightbox(".gallery a");
+  gallery.insertAdjacentHTML('beforeend', markUp);
   lightbox.refresh();
 }
 
 export function clearGallery() {
+  gallery.innerHTML = '';
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createGallery, hideLoader, showLoader, showToast } from './js/render-functions.js';
+import { clearGallery, createGallery, hideLoader, showLoader, showToast } from './js/render-functions.js';
 import { getImagesByQuery } from './js/pixabay-api.js';
 
 const form = document.querySelector("form");
@@ -9,7 +9,7 @@ async function onSubmit(e) {
   e.preventDefault();
   const query = e.target.elements["search-text"].value.trim();
   if (!query) return;
-
+  clearGallery();
   showLoader();
   try {
     const data = await getImagesByQuery(query);
