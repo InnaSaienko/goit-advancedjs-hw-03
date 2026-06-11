cb3a79b0cc8b2b89253a681f7184b7f960977a65
Inna Saienko <inna.saienko.nl@gmail.com>
2026-06-11 16:54:24 +0200

    feat: add SimpleLightbox with captions and custom arrows

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,13 +1,20 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 //Show message form according state
 const messages = {
   warning: "Sorry, there are no images matching your search query. Please try again!",
   error: "An error occurred. Please try again later."
 };
-const lightbox = new SimpleLightbox('.gallery a');
+const svgArrow = `<svg class='icon-arrow' xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\"><path d=\"M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z\"/></svg>`;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 const gallery = document.querySelector('.gallery');
 
 export const showToast = (messageType) => {
@@ -33,6 +40,20 @@ export function hideLoader() {
   loader.remove();
 }
 
+export function showModal () {
+  const prevBtn = document.querySelector('.sl-prev');
+  const nextBtn = document.querySelector('.sl-next');
+
+  if (prevBtn) {
+    prevBtn.innerHTML = svgArrow;
+    prevBtn.querySelector('svg').style.transform = 'rotate(180deg)';
+  }
+
+  if (nextBtn) {
+    nextBtn.innerHTML = svgArrow;
+  }
+};
+
 export function createGallery(images) {
   const markUp = images.map(image => {
     return `<li class="gallery-item">
@@ -54,6 +75,7 @@ export function createGallery(images) {
   }).join('');
 
   gallery.insertAdjacentHTML('beforeend', markUp);
+  lightbox.on("shown.simplelightbox", showModal);
   lightbox.refresh();
 }
 
